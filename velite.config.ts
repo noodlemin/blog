4b57885fd6354c8429924be07a62f96e674fd76b
Minsung Kim <tkfpsk@gmail.com>
2024-07-18 20:41:01 +0100

    velite setup

---
+++ velite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig, defineCollection, s } from 'velite';
+
+//  /blog/hello-world
+const computedFields = <T extends {slug: string}>(data: T) => ({
+  ...data,
+  slugAsParams: data.slug.split("/").slice(1).join("/"),
+});
+
+const posts = defineCollection({
+  name: "Post",
+  pattern: "blog/**/*.mdx",
+  schema: s.object({
+    slug: s.path(), // auto generate slug from file path
+    title: s.string().max(99),
+    description: s.string().max(999).optional(),
+    date: s.isodate(),
+    published: s.boolean().default(true),
+    body: s.mdx()
+  })
+  .transform(computedFields),
+});
+
+export default defineConfig({
+  root: "content",
+  output:{
+    data: ".velite",
+    assets: "public/static",
+    base: "/static/",
+    name: "[name]-[hash:6].[ext]",
+    clean: true
+  },
+  collections: { posts },
+  mdx: {
+    rehypePlugins: [],
+    remarkPlugins: [],
+  }
+})
