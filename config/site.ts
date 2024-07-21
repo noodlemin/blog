c1f1282b46e5cd54e1d2d3b4e960397ee298478c
Minsung Kim <tkfpsk@gmail.com>
2024-07-21 09:32:05 +0100

    Stage 4:Mobile Nav

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "MinBlog",
+  url: "https://example.com",
+  desctiption: "Nextjs 14 blog using velite, tailwind, and shadcn",
+  links:{
+    github: "https://github.com/noodlemin",
+    linkedin: "https://www.linkedin.com/in/minkimdg/",
+    portfolio: "https://minsungkim.pro",
+  },
+};
+
+export type SiteConfig = typeof siteConfig
